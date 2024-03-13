d3a5bc64b4c233daaadfd19ed128c33bb0261cdf
Arnav <arnard76@gmail.com>
2024-03-13 23:34:20 +1300

    barcode scanner working perfectly

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import BarcodeReader from "$lib/BarcodeReader.svelte";
+  import beeper from "$lib/beeper";
+  let barcode: string | null = null;
+
+  const handleBarcode = (data: any) => {
+    if (barcode != data.text) (beeper as HTMLAudioElement).play();
+
+    barcode = data.text;
+  };
+</script>
+
+<BarcodeReader onScan={handleBarcode} />
+<p>{barcode}</p>
